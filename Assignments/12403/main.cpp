8ef5c378e341d1c1be64e604d73ed624e34eb1eb
MaryDavis89 <maryelijah2010@gmail.com>
2020-08-30 22:37:22 -0500

    Create main.cpp

---
+++ Assignments/12403/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+  ifstream infile;
+  infile.open("input2.txt");
+
+string test;
+int c;
+int sum = 0;
+int num = 0;
+infile >> c;
+for(int a = 0; a < c; a++){
+  infile >> test;
+  if(test == "donate"){
+    infile >> num;
+    sum = sum + num;
+    }
+  else
+    cout << sum << endl;
+  }
+ 
+  
+}
